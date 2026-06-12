59507e387c0593055f9d6aecb2ea7acdc85380fa
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-06-12 12:01:12 +0200

    feat(auth): add very basic auth functionality on /auth/login

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  // TODO Replace Record<string, string> with a DTO class to validate the body in a pipe
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: Record<string, string>) {
+    console.log(signInDto);
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  /**
+   * @brief For a username and password, return the associate user object.
+   */
+  async signIn(username: string, pass: string) {
+    const user = await this.usersService.findOne(username);
+    // TODO Compare hashes instead of raw passwords
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+
+    const { password, ...result } = user;
+    // TODO Generate and return a JWT instead of the raw user data
+    return result;
+  }
+}
