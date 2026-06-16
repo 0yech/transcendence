04a9186701fc55edf6f54414ad13f6ebef8f41a8
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-06-16 09:43:25 +0200

    feat(auth): create a jwt token when authentication happens
    This makes the authentication token harder to guess.

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,22 +1,29 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   /**
-   * @brief For a username and password, return the associate user object.
+   * @brief For a username and password, return the associated user object.
    */
-  async signIn(username: string, pass: string) {
+  async signIn(
+    username: string,
+    pass: string,
+  ): Promise<{ accessToken: string }> {
     const user = await this.usersService.findOne(username);
     // TODO Compare hashes instead of raw passwords
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
 
-    const { password, ...result } = user;
-    // TODO Generate and return a JWT instead of the raw user data
-    return result;
+    const payload = { sub: user.id, username: user.username };
+
+    return { accessToken: await this.jwtService.signAsync(payload) };
   }
 }
---
+++ backend/src/auth/constants.ts
@@ -0,0 +1,5 @@
+export const jwtConstants = {
+  // TODO Use a real secret based on .env or docker secrets
+  secret:
+    'wow this is such a great, secure value that nobody can possibly read',
+};
