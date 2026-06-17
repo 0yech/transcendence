480ce9606685ae0751cd6b00f752316ce0bebab8
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-06-17 09:30:21 +0200

    feat(auth): use .env-example variable for jwt library's secret
    This is certainly better than hardcoding the token.

--- backend/src/auth/constants.ts
+++ backend/src/auth/constants.ts
@@ -1,5 +1,5 @@
+import { env } from 'process';
+
 export const jwtConstants = {
-  // TODO Use a real secret based on .env or docker secrets
-  secret:
-    'wow this is such a great, secure value that nobody can possibly read',
+  secret: env.JWT_SECRET, // Secret to be used by the JWT library
 };
