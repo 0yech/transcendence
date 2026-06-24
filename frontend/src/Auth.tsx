2731e491fa735f82ee2c45f0b9deb6073bd5ee97
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-06-24 08:33:10 +0200

    feat(frontend): add barebones login and register pages
    This is very ugly, but it works technically! Still needs quite a bit of
    work, but first, I need to fix the backend expecting a username when we
    send it an email.

---
+++ frontend/src/Auth.tsx
@@ -0,0 +1,51 @@
+export function Login() {
+  return (
+    <>
+      <form name="login" method="post" action="http://localhost:3000/auth/login">
+        <div>
+          <label>
+            Email address
+            <input name="email" type="email" autoComplete="email" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password
+            <input name="password" type="password" autoComplete="password" />
+          </label>
+        </div>
+        <button>Login</button>
+      </form>
+
+      <div>
+        <p>No account? <a href="./register">Register instead</a></p>
+      </div>
+    </>
+  );
+};
+
+export function Register() {
+  return (
+    <>
+      <form name="register" method="post" action="http://localhost:3000/auth/register">
+        <div>
+          <label>
+            Email address
+            <input name="email" type="email" autoComplete="email" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password
+            <input name="password" type="password" autoComplete="password" />
+          </label>
+        </div>
+        <button>Login</button>
+      </form>
+
+      <div>
+        <p>Already have an account? <a href="./login">Login here</a></p>
+      </div>
+    </>
+  );
+};
