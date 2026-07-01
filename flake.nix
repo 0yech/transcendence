97b352b022821213f4a19461ff29d2dfb9b90cd9
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-07-01 14:20:13 +0200

    chore(nix): add nix flake for development environment

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A Nix-flake-based transcendence development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    # system should match the system you are running on
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        gnumake
+        nodejs
+        docker
+      ];
+
+      shellHook = ''
+        echo "draw a card."
+      '';
+    };
+  };
+}
