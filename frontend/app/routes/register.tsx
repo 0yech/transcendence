84ddb8eb074cb8a7db022160e9a98faabeb3a89a
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-07-03 16:38:29 +0200

    feat(register): add barebones register form
    Same state as the login on, this one doesn't do more job than curl, but
    it is integrated in React Router!

---
+++ frontend/app/routes/register.tsx
@@ -0,0 +1,30 @@
+import { RegisterForm } from '../auth/register';
+import type { Route } from '../+types/root';
+
+export async function clientAction({ request }: Route.ActionArgs) {
+  const data = await request.formData();
+  console.log(JSON.stringify(Object.fromEntries(data)));
+  const response = await fetch('http://127.0.0.1:3000/auth/register', {
+    method: 'POST',
+    body: JSON.stringify(Object.fromEntries(data)),
+    headers: new Headers({
+      'Content-Type': 'application/json',
+    }),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status} ${response.statusText}`);
+    }
+
+    return response.blob();
+  });
+}
+
+export default function Login() {
+  return (
+    <>
+      <title>Register to Transcendence</title>
+      <h1>Register to Transcendence</h1>
+      <RegisterForm />
+    </>
+  );
+}
