0570f380e276bc518eeb7d7f8cccaf552520c15b
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-07-07 17:28:18 +0200

    feat(auth): make backend use session_id cookie for authentication
    The AuthGuard guard will now check for the session_id cookie instead of
    the Authentication header.

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -5,7 +5,6 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -13,7 +12,7 @@ export class AuthGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
+    const token = request.cookies['session_id'];
     if (!token) {
       throw new UnauthorizedException();
     }
@@ -25,13 +24,5 @@ export class AuthGuard implements CanActivate {
       throw new UnauthorizedException();
     }
     return true;
-  }
-
-  /**
-   * @brief Extracts the JWT authorization token from the incoming request.
-   */
-  extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    return type === 'Bearer' ? token : undefined;
   }
 }
