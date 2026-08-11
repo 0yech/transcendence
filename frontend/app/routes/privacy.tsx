6e0ae66305ceb3e29d5824b07d4883e72df814e2
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-08-11 07:43:34 +0200

    feat(legal): add barebones privacy and terms of service pages
    The styling and structure was kept to a minimum, so that the frontend
    team can come back and modify this pretty easily.
    
    The text is pretty good, but will need a remaster once we're done with
    the project, adding the new features and especially any further data we
    decided on storing.

---
+++ frontend/app/routes/privacy.tsx
@@ -0,0 +1,10 @@
+import PrivacyPolicy from '~/legal/privacy';
+
+export default function Privacy() {
+  return (
+    <>
+      <title>Privacy Policy</title>
+      <PrivacyPolicy />
+    </>
+  );
+}
