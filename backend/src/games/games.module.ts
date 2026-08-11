33ff3d416e6ba337556046589711a40fe9257447
0yech <Nelson.rey02@gmail.com>
2026-08-11 06:18:18 +0200

    feat(ws-game): change REST logic to websockets for games module

--- backend/src/games/games.module.ts
+++ backend/src/games/games.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+
 import { PrismaModule } from '../prisma/prisma.module';
 import { GamesController } from './games.controller';
+import { GamesGateway } from './games.gateway';
 import { GamesService } from './games.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [GamesController],
-  providers: [GamesService],
+  providers: [
+    GamesService,
+    GamesGateway,
+  ],
 })
 export class GamesModule {}
