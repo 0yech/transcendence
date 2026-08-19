513c1fc8fc7272b554059785cf63241d062f2a03
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-08-19 15:17:51 +0200

    feat(users): add route to update user data

---
+++ backend/src/auth/dto/update.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString, IsUrl, Length } from 'class-validator';
+
+export class UpdateDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  @Length(4, 128)
+  email!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(0, 32)
+  username!: string;
+
+  // @IsNotEmpty()
+  // @IsString()
+  // @Length(8, 64)
+  // password!: string;
+
+  @IsNotEmpty()
+  @IsUrl()
+  pictureUrl!: string;
+}
