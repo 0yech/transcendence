f90586b9cbcd386c9897296548addce26249ea6e
Miniflint <tristan.goel@gmail.com>
2026-08-20 13:41:30 +0200

    feat: added game route, added leave and join lobby, preparing to add context API for /game/:code/play for the game itself

--- frontend/app/utils/api-fetch.ts
+++ frontend/app/utils/api-fetch.ts
@@ -31,3 +31,23 @@ export default async function apiFetch(
 
   return secondTry;
 }
+
+
+export async function handleJoinLobby(code: string) {
+  apiFetch(`/api/lobbies/${code}/join`, {
+    method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).catch((e) => "Not logged in :(" + e);
+};
+
+
+export async function handleLeaveLobby() {
+    apiFetch(`/api/lobbies/leave`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+};
