e188f87604676bc6e1bc9b905c3f8b7d46847c3d
Miniflint <tristan.goel@gmail.com>
2026-08-22 22:42:59 +0200

    fix: creating context api for the game

--- frontend/app/routes/lobby.tsx
+++ frontend/app/routes/lobby.tsx
@@ -1,42 +1,39 @@
 import type { Route } from './+types/profile';
 import apiFetch from '~/utils/api-fetch';
-import type { Params } from "react-router";
+import type { Params } from 'react-router';
 import { DisplayUsers, JoinLobby, LeaveLobby } from '~/utils/lobbies';
 import { useNavigate } from 'react-router';
 
 export async function clientLoader({ params }: { params: Params<string> }) {
-  const { code }  = params;
+  const { code } = params;
   const data = await apiFetch(`/api/lobbies/${code}`);
   return data.json();
 }
 
-export default function PreGame({ loaderData }: Route.ComponentProps)
-{
-  const {id, code, active, createdAt, updatedAt, users, chat} = loaderData;
+export default function PreGame({ loaderData }: Route.ComponentProps) {
+  const { id, code, active, createdAt, updatedAt, users, chat } = loaderData;
   const navigate = useNavigate();
   console.log(users);
   console.log(chat);
   return (
     <>
       <li>
-        <JoinLobby code={code}/>
+        <JoinLobby code={code} />
       </li>
       <li>
         <LeaveLobby />
       </li>
       <li>
-        <button onClick={() => navigate("/")}>
-          Home
-        </button>
+        <button onClick={() => navigate('/')}>Home</button>
       </li>
-      <br/>
+      <br />
       <h2>Id: {id}</h2>
       <h2>Code: {code}</h2>
       <h2>active: {active}</h2>
       <h2>createdAt: {createdAt}</h2>
       <h2>updatedAt: {updatedAt}</h2>
       <h2>Users</h2>
-      <DisplayUsers users={users}/>
+      <DisplayUsers users={users} />
     </>
-  )
+  );
 }
