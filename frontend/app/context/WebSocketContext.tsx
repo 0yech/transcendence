8c3b8e7e92555318cd6d3db0bc0283255d2f3028
Miniflint <tristan.goel@gmail.com>
2026-08-23 14:49:45 +0200

    fix: some progress

--- frontend/app/context/WebSocketContext.tsx
+++ frontend/app/context/WebSocketContext.tsx
@@ -1,9 +1,10 @@
 import { createContext, useRef } from "react";
 import type { ReactNode } from "react";
+import { io, Socket } from 'socket.io-client';
 
 interface InterfaceWSConnection
 {
-  ws: WebSocket | null,
+  ws: Socket | null,
   connect: (code: string) => void,
   disconnect: () => void,
 };
@@ -12,13 +13,25 @@ const WebsocketContext = createContext<InterfaceWSConnection | null>(null);
 
 export function WebSocketRef({children} : {children: ReactNode})
 {
-    const wsRef = useRef<WebSocket | null>(null);
+    const wsRef = useRef<Socket | null>(null);
     function connect(code: string) {
-        wsRef.current = new WebSocket("/game")
+        wsRef.current = io("/socket.io", {
+            reconnection: true
+        });
+        wsRef.current.on("connect", () => console.log("connected to websocket"));
+        wsRef.current.on("disconnect", () => console.log("disconnected"));
+        
+        wsRef.current.emit("game:join", {
+            "lobbyCode": code
+        }, (ack: { ok: boolean; lobbyCode: string }) => console.log(ack));
     }
 
     function disconnect() {
-
+        if (wsRef.current?.connected)
+        {
+            wsRef.current.disconnect();
+            wsRef.current = null;
+        }
     }
     return (
     <>
