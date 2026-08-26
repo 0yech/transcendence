8a0f8b71bae12684912443d366c996ebed4d4e69
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-08-26 12:38:59 +0200

    feat(auth): add user-friendly messages to dto classes' class-validator fields
    These fields will be used to display the error message on the relevant
    front-end page. As such, they need to be better than the default, which
    are machine-generated and not generally user friendly. For example, it'd
    use "higher or equal to 32 characters" instead of "at least 32
    characters".

--- backend/src/auth/dto/update.dto.ts
+++ backend/src/auth/dto/update.dto.ts
@@ -1,23 +1,31 @@
 import { IsEmail, IsNotEmpty, IsString, IsUrl, Length } from 'class-validator';
 
 export class UpdateDto {
-  @IsNotEmpty()
-  @IsString()
-  @IsEmail()
-  @Length(4, 128)
+  @IsNotEmpty({ message: 'Please enter your email address.' })
+  @IsString({ message: 'Your email address must be text.' })
+  @IsEmail({}, { message: 'Please enter a valid email address.' })
+  @Length(4, 128, {
+    message:
+      'Your email address must be between $constraint1 and $constraint2 characters.',
+  })
   email!: string;
 
-  @IsNotEmpty()
-  @IsString()
-  @Length(0, 32)
+  @IsNotEmpty({ message: 'Please choose a username.' })
+  @IsString({ message: 'Your username must be text.' })
+  @Length(0, 32, {
+    message: 'Your username can be at most $constraint2 characters.',
+  })
   username!: string;
 
-  @IsNotEmpty()
-  @IsString()
-  @Length(8, 64)
+  @IsNotEmpty({ message: 'Please choose a password.' })
+  @IsString({ message: 'Your password must be text.' })
+  @Length(8, 64, {
+    message:
+      'Your password must be between $constraint1 and $constraint2 characters.',
+  })
   password!: string;
 
-  @IsNotEmpty()
-  @IsUrl()
+  @IsNotEmpty({ message: 'Please provide a picture URL.' })
+  @IsUrl({}, { message: 'Your picture URL must be a valid web address.' })
   pictureUrl!: string;
 }
