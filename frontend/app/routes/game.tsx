d7164dcdcfccbbe1ad38ee4d0d4c60ee8537baf3
Miniflint <tristan.goel@gmail.com>
2026-08-26 02:59:56 +0200

    feat: welcome page has 'go to active lobby' or 'go to active game'

--- frontend/app/routes/game.tsx
+++ frontend/app/routes/game.tsx
@@ -1,5 +1,27 @@
 import { UseWebSocket } from "~/context/UseWebSocket";
+import { useNavigate } from "react-router";
 
-export default function playGame() {
-  return (<h1>Hello</h1>);
+
+export default function PlayGame() {
+  const { playSlot } = UseWebSocket();
+  const navigate = useNavigate();
+  return (
+    <>
+      <li>
+        <button onClick={() => navigate("/")}>home</button>
+      </li>
+      <li>
+        <button onClick={() => playSlot(1)}>play Slot 1</button>
+      </li>
+      <li>
+        <button onClick={() => playSlot(2)}>play Slot 2</button>
+      </li>
+      <li>
+        <button onClick={() => playSlot(3)}>play Slot 3</button>
+      </li>
+      <li>
+        <button onClick={() => playSlot(4)}>play Slot 4</button>
+      </li>
+    </>
+  );
 }
