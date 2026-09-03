ff9d8758beee45134adc4ce265a0b0fd415a9cf0
Hermès Rolle <hermesrolle@MacBook-Pro-3.local>
2026-09-03 15:14:55 +0200

    feat: add simple profil page and component

--- frontend/app/routes/profile.tsx
+++ frontend/app/routes/profile.tsx
@@ -1,57 +1,16 @@
-import apiFetch from '~/utils/api-fetch';
-import { LogoutButton } from '~/pages/auth/logout';
-import { RemoveAccountButton } from '~/pages/auth/remove-account';
-import type { Route } from './+types/profile';
-import { HomeButton } from './home';
-import { UseWebSocket } from '~/context/UseWebSocket';
-import { useNavigate } from 'react-router';
-import ProfilePicture from '~/components/ProfilePicture';
-import { NavBar } from '../components/Navbar';
+import { useState, useEffect } from 'react';
+import type { SelfUserInterface } from '~/context/WebSocketContext';
+import { UserProfile } from '~/components/userProfiles/userProfile';
 
-export async function clientLoader() {
-  const data = await apiFetch('/api/auth/me');
-  return data.json();
-}
+export default function Profile() {
+  const [user, setUser] = useState<SelfUserInterface | null>(null);
 
-export default function Profile({ loaderData }: Route.ComponentProps) {
-  const {
-    id,
-    username,
-    avatarUrl,
-    email,
-    guild,
-    // lobbyId,
-    // gamePlayers,
-    // totalPts,
-    // sentGuildInvitations,
-    // createdAt
-  } = loaderData;
-  const { setUserId, userId } = UseWebSocket();
-  const navigate = useNavigate();
-  setUserId(id);
-  console.log(userId());
-  console.log(loaderData);
-  return (
-    <>
-      <title>{username}'s Profile</title>
-      <NavBar></NavBar>
-      <h1 className="text-3xl font-bold">{username}'s Profile</h1>
-      <ProfilePicture avatarUrl={avatarUrl} username={username} />
+  useEffect(() => {
+    fetch('/api/auth/me')
+      .then((data) => data.json())
+      .then((json) => setUser(json))
+      .catch((e) => console.log(e));
+  }, []);
 
-      <LogoutButton />
-
-      <HomeButton />
-
-      <h2 className="text-2xl font-bold">Email</h2>
-      <div>{email}</div>
-
-      <h2 className="text-2xl font-bold">Current Guild</h2>
-      <li>
-        <button onClick={() => navigate('/guilds/me')}>
-          {guild?.name || 'No active guild'}
-        </button>
-      </li>
-      <RemoveAccountButton />
-    </>
-  );
+  return <UserProfile user={user} />;
 }
